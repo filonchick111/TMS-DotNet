4dd33d6f93b12d12b0c32c8039fdd7c9600ffb80
Yulia Filon <62154346+filonchick111@users.noreply.github.com>
2020-03-20 18:09:48 +0300

    feat: create new project Filon.TSM206 feat: create .gitignore file init: deploy init: docs

---
+++ src/Filon.TSM206/Filon.TSM206/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Filon.TSM206
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Yuliya!");
+            Console.ReadLine();
+        }
+    }
+}
