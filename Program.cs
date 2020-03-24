3ba629f628f6b7b5e835cd27b89c2ea40978f49d
Yulia Filon <62154346+filonchick111@users.noreply.github.com>
2020-03-24 16:33:57 +0300

    simplecalc

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace SimpleCalc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+          
+        
+        {
+           
+            double a;
+            double b;
+            double total;
+            char oper;
+            Console.WriteLine("Введите первое число");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите оператор:");
+            oper = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("введите второе число");
+            b = Convert.ToDouble(Console.ReadLine());
+            if (oper == '+')
+            {
+                total = a + b;
+                Console.WriteLine(" Сумма " + a + " и " + b + " равна " + total + ".");
+
+            }
+            else if (oper == '-')
+            {
+                total = a - b;
+                Console.WriteLine("Разность " + a + " и " + b + " равна " + total + ".");
+
+            }
+            else if (oper == '*')
+            {
+                total = a * b;
+                Console.WriteLine("Умножение " + a + " и " + b + " равно " + total + ".");
+            }
+            else if (oper == '/')
+            {
+                total = a / b;
+                Console.WriteLine("Деление " + a + " и " + b + " равно " + total + ".");
+            }
+            else
+            {
+                Console.WriteLine("Неизвестный оператор.");
+
+            }
+
+            Console.ReadLine();
+
+
+        }
+
+
+    }
+}
